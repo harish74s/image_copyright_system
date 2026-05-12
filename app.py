f92489386b18e10714b4d431f69bfc398a9e2071
harish <harishkumaran74s@gmail.com>
2026-05-12 20:38:41 +0530

    added image comparision feature

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import os
+from PIL import Image
+import imagehash
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'static/images'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route('/')
 def home():
-    return "Image Copyright Detection System Running"
+    return render_template('index.html')
+
+@app.route('/compare', methods=['POST'])
+def compare():
+
+    image1 = request.files['image1']
+    image2 = request.files['image2']
+
+    path1 = os.path.join(app.config['UPLOAD_FOLDER'], image1.filename)
+    path2 = os.path.join(app.config['UPLOAD_FOLDER'], image2.filename)
+
+    image1.save(path1)
+    image2.save(path2)
+
+    hash1 = imagehash.average_hash(Image.open(path1))
+    hash2 = imagehash.average_hash(Image.open(path2))
+
+    difference = hash1 - hash2
+
+    similarity = max(0, 100 - (difference * 5))
+
+    return render_template(
+        'index.html',
+        score=similarity,
+        image1='/' + path1,
+        image2='/' + path2
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
